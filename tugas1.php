5989e8abad03da59322a951b0702f1762b423afd
Masykur-Harira <55335996+Masykur-Harira@users.noreply.github.com>
2019-09-22 12:00:54 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Selamat Datang</h1>
+</body>
+</html>
+
+<?php 
+$x="Nama:";
+$x.="Masykur ";
+$x.="Harira";
+echo $x;
+?>
+
+<?php
+echo "<br>";
+echo "Segitiga"
+?>
+
+<?php
+$y="3";
+echo "<br>";
+echo "Alas=".$y;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas=".$y*$y/2;
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+echo "Lingkaran";
+?>
+
+<?php
+$x="3.14";
+$y="7";
+echo "<br>";
+echo "Jari-Jari=".$y;
+echo "<br>";
+echo "phi=".$x;
+echo "<br>";
+echo "luas=".$x*$y*$y;
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+echo "Jajar Genjang";
+?>
+
+<?php
+$y="3";
+echo "<br>";
+echo "Alas=".$y;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas=".$y*$y;
+?>
